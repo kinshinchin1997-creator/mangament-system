2be3c5aefb2af88dae4a19cbef9031148f004d75
Developer <dev@example.com>
2025-12-27 14:57:35 +0800

    fix: add PaymentModule to ContractModule imports for dependency injection

--- src/modules/contract/contract.module.ts
+++ src/modules/contract/contract.module.ts
@@ -2,9 +2,13 @@ import { Module, forwardRef } from '@nestjs/common';
 import { ContractController } from './contract.controller';
 import { ContractService } from './contract.service';
 import { FinanceModule } from '../finance/finance.module';
+import { PaymentModule } from '../payment/payment.module';
 
 @Module({
-  imports: [forwardRef(() => FinanceModule)],
+  imports: [
+    forwardRef(() => FinanceModule),
+    forwardRef(() => PaymentModule),
+  ],
   controllers: [ContractController],
   providers: [ContractService],
   exports: [ContractService],
